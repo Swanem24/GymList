08c7282668c8e6962d278a640ac2d583afbb55c1
Sanjog Wanem <sjwanem@gmail.com>
2023-08-06 10:07:54 +0100

    06/08/2023

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-var userInput = document.getElementById("inputList").value;
+function closetheitem() {
+    const element = document.querySelector(".checked");
+    element.remove();
+}
 
 function newItem() {
     //  Create an new li node
     var node = document.createElement("li");
+    var userInput = document.getElementById("inputList").value;
 
     //  Create a text node or get it from the input
-    var textnode = document.createTextNode(document.getElementById("inputList").value);
+    var textnode = document.createTextNode(userInput);
 
     //  Append the text node to li node
-    if(textnode === '') {
-        console.log("you havent wrote anything down");
+    node.appendChild(textnode);
+
+    if(userInput === '') {
+        alert("You havent wrote anything!");
     } else {
         console.log("You wrote " + textnode);
-        node.appendChild(textnode);
         document.getElementById("list").appendChild(node);
     }
 
-    document.getElementById("inputList").value = " ";
+    document.getElementById("inputList").value = "";
 }
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+    ev.target.classList.toggle('checked');
+    ev.target.id = 'delete';
+  }
+);
